aa9fc8e9b2a599b965894b2597a648104c4f34ab
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-02-08 15:07:04 +0300

    Maske Takip

---
+++ MaskeTakip/Business/Abstract/IApplicantService.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IApplicantService // Interface'de sadece imzalar olur. Method içi boş kalır.
+    {
+
+        void ApplyForMask(Person person); // Maske için başvur
+
+
+
+        List<Person> GetList();
+
+
+
+        bool CheckPerson(Person person);
+
+    }
+       
+    
+}
+
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,50 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using static Workaround.Program;
+
+namespace Workaround;
+
+class Program
+
+{
+
+    static void Main(String[] args)
+    {
+        // Vatandas vatandas1 = new Vatandas(); // Instance >> Vatandaş oluşturma
+
+        SelamVer(isim:"Yiğit");
+        SelamVer(isim: "Ahmet");
+        SelamVer(); 
+
+        Person person1 = new Person ();  
+        person1.FirstName = "Engin";
+
+        Person person2 = new Person();
+        person2.FirstName = "Murat";
+
+        int sonuc = Topla(3,5);
+
+        PttManager pttManager = new PttManager(new ForeignManager());
+
+        pttManager.GiveMask(person1);
+
+    }
+    // Method
+    static void SelamVer(string isim="isimsiz")
+    {
+        Console.WriteLine(" Merhaba " +isim);
+
+
+    }
+
+    static int Topla(int sayi1, int sayi2)
+    {
+        int sonuc = sayi1 + sayi2;
+        Console.WriteLine("Toplam " + sonuc);    
+        return sonuc;
+    }
+
+
+
+}   
