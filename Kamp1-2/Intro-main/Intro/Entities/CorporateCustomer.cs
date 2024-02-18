997261136caf87630f7b8410ebc68c4a8f43b20d
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-02-19 01:33:07 +0300

    2024 Yazılım Geliştirici Yetiştirme Kampı 1-2.Gün
    2024 C# Kampı 1. ve 2. Gün

---
+++ Kamp1-2/Intro-main/Intro/Entities/CorporateCustomer.cs
@@ -0,0 +1,9 @@
+﻿namespace Intro.Entities
+{
+    public class CorporateCustomer:BaseCustomer // Firma
+    {
+        public string Name { get; set; }
+        public string TaxNumber { get; set; } // Vergi numarası
+
+    }
+}
